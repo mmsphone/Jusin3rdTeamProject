89c707466f6785709d3e6a49d73b10a03a5c6d6a
mmsphone <155417088+mmsphone@users.noreply.github.com>
2025-05-26 23:09:27 +0900

    메뉴씬

---
+++ WindowsProject1/MenuScene.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Scene.h"
+
+class MenuScene : public Scene {
+public:
+    virtual void Load() override;
+    virtual void Unload() override;
+    virtual void Render(HDC hdc) override;
+    virtual void Update(double dt) override;
+
+private:
+    enum class MenuState {
+        MAIN,
+        SELECT
+    } 
+    eMenuState = MenuState::MAIN;
+
+    void CreateMainMenu();
+    void CreateGameSelectMenu();
+};
