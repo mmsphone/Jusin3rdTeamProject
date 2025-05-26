0f4fdbc50550868088c9844657584e7621970626
mmsphone <155417088+mmsphone@users.noreply.github.com>
2025-05-26 23:08:54 +0900

    텍스쳐매니저

---
+++ WindowsProject1/TextureManager.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include "TextureManager.h"
+
+void TextureManager::InsertTexture(const std::wstring& filePath, const std::wstring& key) {
+    if (textureMap.find(key) != textureMap.end())
+        return;
+
+    HBITMAP hBitmap = (HBITMAP)LoadImageW(
+        nullptr, filePath.c_str(), IMAGE_BITMAP, 0, 0,
+        LR_LOADFROMFILE | LR_CREATEDIBSECTION
+    );
+
+    if (!hBitmap) {
+        OutputDebugStringW((L"텍스처 로드 실패: " + filePath + L"\n").c_str());
+        return;
+    }
+
+    HDC hdc = CreateCompatibleDC(nullptr);
+    HBITMAP hOld = (HBITMAP)SelectObject(hdc, hBitmap);
+
+    TextureData data;
+    data.hdc = hdc;
+    data.hBitmap = hBitmap;
+    data.hOldBitmap = hOld;
+
+    textureMap[key] = data;
+
+#ifdef _DEBUG
+    OutputDebugStringW((L"텍스처 로드 성공: " + key + L"\n").c_str());
+#endif
+}
+
+HDC TextureManager::FindTexture(const std::wstring& key) const {
+    auto it = textureMap.find(key);
+    if (it != textureMap.end())
+        return it->second.hdc;
+
+    return nullptr;
+}
+
+void TextureManager::Release() {
+    for (auto& [key, data] : textureMap) {
+        if (data.hdc && data.hOldBitmap)
+            SelectObject(data.hdc, data.hOldBitmap);
+        if (data.hBitmap)
+            DeleteObject(data.hBitmap);
+        if (data.hdc)
+            DeleteDC(data.hdc);
+    }
+    textureMap.clear();
+}
+
+TextureManager::~TextureManager() {
+    Release();
+}
---
+++ WindowsProject1/TextureManager.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+class TextureManager {
+public:
+    TextureManager() noexcept = default;
+    ~TextureManager();
+
+    TextureManager(const TextureManager&) = delete;
+    TextureManager& operator=(const TextureManager&) = delete;
+    TextureManager(TextureManager&&) = delete;
+    TextureManager& operator=(TextureManager&&) = delete;
+
+    void InsertTexture(const std::wstring& filePath, const std::wstring& key);
+    HDC FindTexture(const std::wstring& key) const;
+    void Release();
+
+private:
+    struct TextureData {
+        HDC hdc = nullptr;
+        HBITMAP hBitmap = nullptr;
+        HBITMAP hOldBitmap = nullptr;
+    };
+
+    std::unordered_map<std::wstring, TextureData> textureMap;
+};
